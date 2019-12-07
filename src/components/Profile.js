9c9ad2acfc6771a142586064528ffb164b5cff71
marco mariscal <marco.a.mariscal@gmail.com>
2019-12-07 18:28:29 -0500

    hackathon app

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import { UserSession, Person } from 'blockstack'
-import NavBar from './NavBar'
-import {jsonCopy, remove, add, check} from '../assets/utils'
-import { appConfig, TASKS_FILENAME } from '../assets/constants'
-import '../styles/Profile.css'
+import React, { Component } from "react";
+import { UserSession, Person } from "blockstack";
+import NavBar from "./NavBar";
+import { jsonCopy, remove, add, check } from "../assets/utils";
+import { appConfig, TASKS_FILENAME } from "../assets/constants";
+import "../styles/Profile.css";
 
 class Profile extends Component {
   constructor(props) {
-  	super(props);
+    super(props);
 
-  	this.state = {
+    this.state = {
       tasks: [],
-      value: '',
-    }
+      value: ""
+    };
 
     this.loadTasks = this.loadTasks.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -27,7 +27,7 @@ class Profile extends Component {
 
   componentWillReceiveProps(nextProps) {
     const nextTasks = nextProps.tasks;
-    if(nextTasks) {
+    if (nextTasks) {
       if (nextTasks.length !== this.state.tasks.length) {
         this.setState({ tasks: jsonCopy(nextTasks) });
       }
@@ -36,23 +36,26 @@ class Profile extends Component {
 
   loadTasks() {
     const options = { decrypt: true };
-    this.props.userSession.getFile(TASKS_FILENAME, options)
-    .then((content) => {
-      if(content) {
+    this.props.userSession.getFile(TASKS_FILENAME, options).then(content => {
+      if (content) {
         const tasks = JSON.parse(content);
-        this.setState({tasks});
-      } 
-    })
+        this.setState({ tasks });
+      }
+    });
   }
 
   saveTasks(tasks) {
     const options = { encrypt: true };
-    this.props.userSession.putFile(TASKS_FILENAME, JSON.stringify(tasks), options);
+    this.props.userSession.putFile(
+      TASKS_FILENAME,
+      JSON.stringify(tasks),
+      options
+    );
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
-   }
+    this.setState({ value: event.target.value });
+  }
 
   removeTask(e) {
     e.preventDefault();
@@ -65,7 +68,7 @@ class Profile extends Component {
   addTask(e) {
     e.preventDefault();
     const tasks = add(this.state);
-    this.setState({value: '', tasks});
+    this.setState({ value: "", tasks });
     this.saveTasks(tasks);
   }
 
@@ -81,18 +84,20 @@ class Profile extends Component {
     const person = new Person(profile);
     return (
       <div className="Dashboard">
-      <NavBar username={username} user={person} signOut={this.props.handleSignOut}/>
-        <div className="row justify-content-center"id="header">
-          <h3 className="user-info">
-            {username}'s to-dos
-          </h3>
+        <NavBar
+          username={username}
+          user={person}
+          signOut={this.props.handleSignOut}
+        />
+        <div className="row justify-content-center" id="header">
+          <h3 className="user-info">{username}'s shared songs</h3>
         </div>
         <br></br>
         <div className="row justify-content-center">
           <div
             id="addTask"
             className="frame"
-            style={{borderColor: '#f8f9fa'}}
+            style={{ borderColor: "#f8f9fa" }}
           >
             <form onSubmit={this.addTask} className="input-group">
               <input
@@ -101,43 +106,69 @@ class Profile extends Component {
                 onChange={this.handleChange}
                 value={this.state.value}
                 required
-                placeholder="To-do..."
+                placeholder="song name"
                 autoFocus={true}
               />
               <div className="input-group-append" id="add-task">
-                <input type="submit" className="btn btn-primary" value="Add"/>
+                <input type="submit" className="btn btn-primary" value="" />
               </div>
             </form>
-            </div>
+
+            <form onSubmit={this.addTask} className="input-group">
+              <input
+                className="form-control"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.value}
+                required
+                placeholder="song link"
+                autoFocus={true}
+              />
+              <div className="input-group-append" id="add-task">
+                <input type="submit" className="btn btn-primary" value="Add" />
+              </div>
+            </form>
           </div>
+        </div>
         <br></br>
         <div className="row justify-content-center">
           <div className="frame">
-            {this.state.tasks.map((task, i) =>
+            {this.state.tasks.map((task, i) => (
               <ul key={i}>
                 <div className="row">
-                  <input type="checkbox" className="form-check-input" data-index={i} onClick={this.checkTask} checked={task[1]? true : false}></input>
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    data-index={i}
+                    onClick={this.checkTask}
+                    checked={task[1] ? true : false}
+                  ></input>
                   <div className="col">
                     <span className="input-group-text">
                       <div className="task">
-                        {task[1]? <s>{task[0]}</s> : task[0]}
-                      </div> 
+                        {task[1] ? <s>{task[0]}</s> : task[0]}
+                      </div>
                       <div className="delete">
-                        <button className="btn btn-primary" data-index={i} onClick={this.removeTask}>
-                          <div className="X" data-index={i}>X</div>
+                        <button
+                          className="btn btn-primary"
+                          data-index={i}
+                          onClick={this.removeTask}
+                        >
+                          <div className="X" data-index={i}>
+                            X
+                          </div>
                         </button>
                       </div>
                     </span>
-                    </div>
                   </div>
+                </div>
               </ul>
-            )}
+            ))}
           </div>
-      </div> 
-    </div>
-  );
+        </div>
+      </div>
+    );
   }
-
 }
 
 // Made this a default prop (instead of using this.userSession) so a dummy userSession
@@ -146,4 +177,4 @@ Profile.defaultProps = {
   userSession: new UserSession(appConfig)
 };
 
-export default Profile
+export default Profile;
